2da60238cf2aa2d94cf0f26cdaa0506123653a4d
kyrdzik <cocf364@gmail.com>
2026-04-06 15:58:12 +0200

    Fixed the wallpaper

--- hypr/wallpaper.sh
+++ hypr/wallpaper.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-WALLPAPERS=(
-    ~/Pictures/arch.png
-    ~/Pictures/arch2.jpg
-    ~/Pictures/arch3.png
-    ~/Pictures/arch4.jpg
-)
-
-# Initialize awww daemon
 awww-daemon &
 sleep 1
-
-while true; do
-    for wall in "${WALLPAPERS[@]}"; do
-        awww img "$wall" \
-            --transition-type fade \
-            --transition-duration 2 \
-            --transition-fps 60
-        sleep 20
-    done
-done
+awww img ~/Pictures/arch4.jpg --transition-type fade --transition-duration 2 --transition-fps 60
